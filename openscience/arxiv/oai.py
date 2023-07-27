bd60aa0aa96fc40bd1fb04efd2135bf80a4ca167
Michael <mtartre@gmail.com>
2023-07-27 16:09:43 -0500

    oai more metadata and ListSets

--- openscience/arxiv/oai.py
+++ openscience/arxiv/oai.py
@@ -1,87 +1,125 @@
+# pylint: disable=missing-function-docstring,missing-class-docstring,invalid-name
 """
 TODO:
- - harmonize in fields from metadataPrefix=arXiv
  - easy way to iterate through everything?
 """
 
 from functools import cached_property
 import typing as t
 from datetime import date
-from dataclasses import field
 import asyncio
 import time
 
-import bs4
 import aiohttp
-from pydantic.dataclasses import dataclass
 from tqdm.auto import tqdm
+import pydantic
+import xmltodict
 
 VALID_METADATA_PREFIXES = ("oai_dc", "arXiv")
-PREFIX_DEFAULT = "oai_dc"
+PREFIX_DEFAULT = "arXiv"
 PAGING_BACKOFF = 4
 
-def get_text(soup: bs4.BeautifulSoup, find: str, required: bool=False) -> t.Optional[str]:
-    if match := soup.find(find):
-        return match.text
-    if required:
-        raise RuntimeError(f"Tag {find} not found.")
+# deprecated pydantic v1 utils
+def to_camel(string: str) -> str:
+    return ''.join(word.capitalize() for word in string.split('_'))
 
-def get_texts(soup: bs4.BeautifulSoup, find: str) -> list[str]:
-    return [
-        match.text for match in soup.find_all(find)
-    ]
+def to_lower_camel(string: str) -> str:
+    if len(string) >= 1:
+        pascal_string = to_camel(string)
+        return pascal_string[0].lower() + pascal_string[1:]
+    return string.lower()
 
-@dataclass
-class OAIMetadata:
-    oai_identifier: t.Optional[str]
-    oai_specs: list[str]
-    oai_datestamp: t.Optional[date]
-    title: str
-    creators: list[str]
-    subjects: list[str]
-    descriptions: list[str] = field(repr=False)
-    dates: list[date]
-    identifiers: list[str]
+class Set(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(alias_generator=to_lower_camel)
+    set_spec: str
+    set_name: str
 
+class DublinCoreMetadata(pydantic.BaseModel):
+    # spec at http://www.openarchives.org/OAI/2.0/oai_dc.xsd
+    model_config = pydantic.ConfigDict(
+        alias_generator=lambda s: 'dc:' + s.rstrip("_").replace("_", "-"))
+    title: str | list[str] | None = None
+    creator: str | list[str] | None = None
+    subject: str | list[str] | None = None
+    description: str | list[str] | None = None
+    publisher: str | list[str] | None = None
+    contributor: str | list[str] | None = None
+    date: str | list[str] | None = None
+    type_: str | list[str] | None = None
+    format_: str | list[str] | None = None
+    identifier: str | list[str] | None = None
+    source: str | list[str] | None = None
+    language: str | list[str] | None = None
+    relation: str | list[str] | None = None
+    coverage: str | list[str] | None = None
+    rights: str | list[str] | None = None
+
+class arXivAuthor(pydantic.BaseModel):
+    keyname: str | None = None
+    forenames: str | None = None
+
+class arXivMetadata(pydantic.BaseModel):
+    # spec at https://arxiv.org/OAI/arXiv.xsd
+    model_config = pydantic.ConfigDict(alias_generator=lambda s: s.rstrip("_").replace("_", "-"))
+    id_: str
+    created: str | None = None
+    updated: str | None = None
+    authors: arXivAuthor | list[arXivAuthor] | None = None
+    title: str | None = None
+    msc_class: str | None = None
+    acm_class: str | None = None
+    report_no: str | None = None
+    journal_ref: str | None = None
+    comments: str | None = None
+    abstract: str | None = None
+    categories: str | None = None
+    doi: str | None = None
+    proxy: str | None = None
+    license_: str | None = None
+
+class Header(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(alias_generator=to_lower_camel)
+    identifier: str | None = None
+    set_spec: str | list[str] | None = None
+    datestamp: date | None = None
+
+class MetadataRecord(pydantic.BaseModel):
+    header: Header
+    metadata: arXivMetadata | DublinCoreMetadata
     @classmethod
-    def from_soup(
-        cls,
-        soup: bs4.BeautifulSoup,
-        prefix: t.Literal[VALID_METADATA_PREFIXES],
-    ) -> 'OAIMetadata':
-        # TODO: harmonize prefixes
-        header = soup.find("header")
-        if prefix == "arXiv":
-            metadata = soup.find("metadata").find("arXiv")
-        elif prefix == "oai_dc":
-            metadata = soup.find("metadata").find("oai_dc:dc")
-        return cls(
-            oai_identifier=get_text(header, "identifier"),
-            oai_specs=get_texts(header, "setSpec"),
-            oai_datestamp=get_text(header, "datestamp"),
-            title=get_text(metadata, "title"),
-            creators=get_texts(metadata, "creator"),
-            subjects=get_texts(metadata, "subject"),
-            descriptions=get_texts(metadata, "description"),
-            dates=get_texts(metadata, "date"),
-            identifiers=get_texts(metadata, "identifier"),
-        )
+    def from_record(cls, record: dict, prefix: t.Literal['arXiv', 'oai_dc']):
+        if prefix == 'arXiv':
+            metadata = record['metadata']['arXiv']
+            metadata |= {'authors': metadata['authors']['author']}
+            metadata = arXivMetadata(**metadata)
+        elif prefix == 'oai_dc':
+            metadata = record['metadata']['oai_dc:dc']
+            metadata = {k: v for k, v in metadata.items() if not k.startswith('@')}
+            metadata = DublinCoreMetadata(**metadata)
+        return cls(header=record['header'], metadata=metadata)
 
 class ArxivOAIClient:
     def __init__(self, url: str="https://export.arxiv.org/oai2", concurrency_limit: int=3):
         self.url = url
-        self.concurrency_limit = 3
+        self.concurrency_limit = concurrency_limit
 
     @cached_property
     def session(self):
         connector = aiohttp.TCPConnector(limit_per_host=self.concurrency_limit)
         return aiohttp.ClientSession(connector=connector)
 
+    async def list_sets(self) -> list[Set]:
+        params = {"verb": "ListSets"}
+        async with self.session.get(self.url, params=params, raise_for_status=True) as resp:
+            text = await resp.text()
+            results = xmltodict.parse(text)['OAI-PMH']['ListSets']['set']
+            return [Set(**result) for result in results]
+
     async def get_record(
         self,
         identifier: str,
         prefix: t.Literal[VALID_METADATA_PREFIXES]=PREFIX_DEFAULT,
-    ) -> OAIMetadata:
+    ) -> MetadataRecord:
         assert prefix in VALID_METADATA_PREFIXES
         params = {
             "verb": "GetRecord",
@@ -90,9 +128,9 @@ class ArxivOAIClient:
         }
         async with self.session.get(self.url, params=params, raise_for_status=True) as resp:
             text = await resp.text()
-            soup = bs4.BeautifulSoup(text, "xml")
-            record = soup.find("GetRecord").find("record")
-            return OAIMetadata.from_soup(record, prefix=prefix)
+            data = xmltodict.parse(text)
+            record = data['OAI-PMH']['GetRecord']['record']
+            return MetadataRecord.from_record(record, prefix=prefix)
 
     async def list_records(
         self,
@@ -101,7 +139,7 @@ class ArxivOAIClient:
         set_: t.Optional[str]=None,
         prefix: t.Literal[VALID_METADATA_PREFIXES]=PREFIX_DEFAULT,
         progress: bool=False,
-    ) -> t.AsyncIterator[OAIMetadata]:
+    ) -> t.AsyncIterator[MetadataRecord]:
         assert prefix in VALID_METADATA_PREFIXES
         params = {
             "verb": "ListRecords",
@@ -113,20 +151,24 @@ class ArxivOAIClient:
             params["set"] = set_
         with tqdm(disable=not progress) as pbar:
             while True:
-                async with self.session.get(self.url, params=params, raise_for_status=True) as resp:
+                async with self.session.get(self.url, params=params) as resp:
+                    if resp.status == 503:
+                        await asyncio.sleep(5)
+                        continue
+                    resp.raise_for_status()
                     response_t = time.perf_counter()
-                    text = await resp.text()
-                    soup = bs4.BeautifulSoup(text, "xml")
-                    resumption_token = soup.find("resumptionToken")
+                    data = xmltodict.parse(await resp.text())
+                    list_records = data['OAI-PMH']['ListRecords']
+                    resumption_token = list_records.get('resumptionToken')
                     if pbar.total is None and resumption_token:
-                        pbar.reset(total=int(resumption_token.attrs["completeListSize"]))
-                    for record in soup.find("ListRecords").find_all("record"):
-                        yield OAIMetadata.from_soup(record, prefix=prefix)
+                        pbar.reset(total=int(resumption_token['@completeListSize']))
+                    for record in list_records["record"]:
+                        yield MetadataRecord.from_record(record, prefix=prefix)
                         pbar.update()
-                    if resumption_token and resumption_token.text:
+                    if resumption_token and resumption_token.get('#text'):
                         params = {
                             "verb": "ListRecords",
-                            "resumptionToken": resumption_token.text,
+                            "resumptionToken": resumption_token.get('#text'),
                         }
                         # wait one more second from the end of prev request
                         #  but include any time taken iterating through results
